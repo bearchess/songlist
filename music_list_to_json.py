f16f45c0a764ce99f55f0fd4b765457b71d6ddda
berachess <744174937@qq.com>
2023-02-28 19:20:17 +0800

    修改Nan错误

--- music_list_to_json.py
+++ music_list_to_json.py
@@ -1,12 +1,15 @@
 import json
-import pandas as pd
+import re
 import urllib.request
-import openpyxl
+
+import pandas as pd
+import simplejson as json
+
 song_list = []
 
 
 def getSongListUrl():
-    url = "https://www.kdocs.cn/api/office/file/crNZPpbHA70t/download"
+    url = "https://www.kdocs.cn/api/office/file/chplCc7GBmi9/download"
     try:
         req = urllib.request.Request(url, method="GET")
         resp = urllib.request.urlopen(req)
@@ -16,7 +19,7 @@ def getSongListUrl():
     except:
         print("Error obtaining song list URL")
         exit()
-    
+
     # if jsonObj["result"] != "ok":
     #     logging.info("getSongListUrl err: fail to get download url")
     #     # 这里可以加一个重试的逻辑哈
@@ -46,15 +49,9 @@ def parseSonglist():
 
 
 if __name__ == '__main__':
-    # 可以当个定时任务跑，最好多打点日志，不然坏了也不知道
-    # while True:
-    #     updateSongList()
-    #     parseSonglist()
-    #     with open("./public/music_list_7.json", 'w') as file:
-    #         file.write(json.dumps(song_list))
-    #     time.sleep(60*60*24)
-
-    #updateSongList()
+    updateSongList()
     parseSonglist()
+    regex = re.compile(r'\bnan\b', flags=re.IGNORECASE)
+    re.sub(regex, ' null ', "{'myKey': nan}")
     with open("./public/music_list_7.json", 'w') as file:
-        file.write(json.dumps(song_list))
+        file.write(json.dumps(song_list, ignore_nan=True))
